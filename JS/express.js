89895cdc3480d987aa9ff9536ceca5b5d5d983f0
Covarel Camille <covarelcamille@gmail.com>
2025-03-26 11:45:29 +0100

    0.2.0 API WORK

--- JS/express.js
+++ JS/express.js
@@ -1,32 +1,57 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
-const data = require("../DATA/data.json");
-
-app.use(express.json());
 
 app.listen(4000, () => {
 	console.log("Serveur à l'écoute");
 });
-/*
-app.get("/post:username", (req, res) => {
-	res.status(200).json(username);
-});
-*/
 
-app.get("/post:id", (req, res) => {
+function getDataPropertyById(propertyName, id) {
+	return (req, res) => {
+		const absolutePath = "../DATA/data.json"; // Remplacez par le chemin absolu correct
+		fs.readFile(absolutePath, "utf8", (err, data) => {
+		  if (err) {
+			console.error(err);
+			res.status(500).send("Erreur lors de la lecture du fichier JSON");
+			return;
+		}
+		try {
+		  const jsonData = JSON.parse(data);
+		  const item = jsonData.find((item) => item.id === id);
+		  if (item && item[propertyName]) {
+			res.send(item[propertyName]);
+		  } else {
+			res.status(404).send(`${propertyName} non trouvé pour l'id ${id}`);
+		  }
+		} catch (parseError) {
+		  console.error(parseError);
+		  res.status(500).send("Erreur lors de l'analyse du fichier JSON");
+		}
+	  });
+	};
+  }
+
+  app.get("/useravatar/:id", (req, res) => {
 	const id = req.params.id;
-	const post = data.filter((p) => p.id === id);
-	res.send(post[0]);
-});
+	getDataPropertyById("useravatar", id)(req, res);
+  });
 
-app.get("/post:userdesc", (req, res) => {
-	res.status(200).json(userdesc);
-});
+  app.get("/username/:id", (req, res) => {
+	const id = req.params.id;
+	getDataPropertyById("username", id)(req, res);
+  });
 
-app.get("/post:postimg", (req, res) => {
-	res.status(200).json(postimg);
-});
+  app.get("/userdesc/:id", (req, res) => {
+	const id = req.params.id;
+	getDataPropertyById("userdesc", id)(req, res);
+  });
 
-app.get("/post:posttext", (req, res) => {
-	res.status(200).json(posttext);
-});
+  app.get("/postimg/:id", (req, res) => {
+	const id = req.params.id;
+	getDataPropertyById("postimg", id)(req, res);
+  });
+
+  app.get("/posttext/:id", (req, res) => {
+	const id = req.params.id;
+	getDataPropertyById("posttext", id)(req, res);
+  });
