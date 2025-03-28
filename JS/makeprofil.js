0625a6bf9474dc07385d4fdb83efe135dab6b5e0
Covarel Camille <covarelcamille@gmail.com>
2025-03-28 15:00:35 +0100

    0.6.23 |    patch profil generation

--- JS/makeprofil.js
+++ JS/makeprofil.js
@@ -16,11 +16,17 @@ function makeprofil(post) {
 	const new_ul = document.createElement("ul");
 
 	const new_li_add = document.createElement("add");
-	new_li_add.classList.add("like");
+	new_li_add.classList.add("add");
 	const new_li_messages = document.createElement("li");
 	new_li_messages.classList.add("messages");
 	const new_li_blockuser = document.createElement("li");
 	new_li_blockuser.classList.add("blockuser");
+	const new_li_addimg = document.createElement("img");
+	new_li_addimg.classList.add("adduser");
+	const new_li_messagesimg = document.createElement("img");
+	new_li_messagesimg.classList.add("messages");
+	const new_li_blockuserimg = document.createElement("img");
+	new_li_blockuserimg.classList.add("blockuser");
 
 
 	// data
@@ -28,9 +34,9 @@ function makeprofil(post) {
 	new_useravatar.src = "./IMG/Nav[1]User.png";
 	new_username.textContent = post.username;
 	new_userdesc.textContent = post.userdesc;
-	new_li_add.src = "./IMG/profil[1]adduser.png";
-	new_li_messages.src = "./IMG/Nav[2]Messages.png";
-	new_li_blockuser.src = "./IMG/profil[1]blockuser.png";
+	new_li_addimg.src = "./IMG/profil[1]adduser.png";
+	new_li_messagesimg.src = "./IMG/Nav[2]Messages.png";
+	new_li_blockuserimg.src = "./IMG/profil[1]blockuser.png";
 
 	// parents
 
@@ -44,8 +50,12 @@ function makeprofil(post) {
 		new_profil.appendChild(new_avatarinteraction);
 			new_avatarinteraction.appendChild(new_ul);
 				new_ul.appendChild(new_li_add);
+					new_li_add.appendChild(new_li_addimg);
 				new_ul.appendChild(new_li_messages);
+					new_li_messages.appendChild(new_li_messagesimg);
 				new_ul.appendChild(new_li_blockuser);
+					// bug user block ne s'affiche pas à corriger
+					new_li_blockuser.appendChild(new_li_addimg);
 
 	return new_profil;
 }
